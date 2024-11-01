4ba311b9b54a0fddf33e1e726c54a1c42cd8cb70
YanGraim <yangraim@gmail.com>
2024-11-01 16:32:18 -0300

    feat(controller): adicionar construtor e buscar dados do banco para exibição na tabela

--- EmpresTech/Controllers/EmprestimoController.cs
+++ EmpresTech/Controllers/EmprestimoController.cs
@@ -1,11 +1,21 @@
+using EmpresTech.Data;
+using EmpresTech.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmpresTech.Controllers;
 
 public class EmprestimoController : Controller
 {
+    private readonly ApplicationDbContext _db;
+
+    public EmprestimoController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+        return View(emprestimos);
     }
 }
--- EmpresTech/obj/Debug/net8.0/EmpresTech.AssemblyInfo.cs
+++ EmpresTech/obj/Debug/net8.0/EmpresTech.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmpresTech")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4bc6dd60a354e2f12ffc134e5c384b636b9c76")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmpresTech")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmpresTech")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
